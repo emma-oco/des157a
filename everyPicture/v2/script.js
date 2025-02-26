37192b8b401ed6c4215fa2da6141b9bda8644962
Emma O'Connor <esoconnor@ucdavis.edu>
2025-02-26 15:49:31 -0800

    exp2

---
+++ everyPicture/v2/script.js
@@ -0,0 +1,70 @@
+(function(){
+
+    'use strict';
+     console.log('reading js');
+
+
+    const area1 = document.querySelector('#1-area');
+    const area2 = document.querySelector('#2-area');
+    const area3 = document.querySelector('#3-area');
+    const area4 = document.querySelector('#4-area');
+    const area5 = document.querySelector('#5-area');
+    const area6 = document.querySelector('#6-area');
+    const area7 = document.querySelector('#7-area');
+    const area8 = document.querySelector('#8-area');
+    const area9 = document.querySelector('#9-area');
+
+    area1.addEventListener('mouseover', function(){
+        const myText = document.querySelector('#item')
+        myText.innerHTML = '<p>This is a blurb</p>'
+
+        
+    });
+
+    area2.addEventListener('mouseover', function(){
+        const myText = document.querySelector('#item')
+        myText.innerHTML = '<p>This is a blurb</p>'
+        
+    });
+
+    area3.addEventListener('mouseover', function(){
+        const myText = document.querySelector('#item')
+        myText.innerHTML = '<p>This is a blurb</p>'
+        
+    });
+
+    area4.addEventListener('mouseover', function(){
+        const myText = document.querySelector('#item')
+        myText.innerHTML = '<p>This is a blurb</p>'
+        
+    });
+
+    area5.addEventListener('mouseover', function(){
+        
+    });
+
+    area6.addEventListener('mouseover', function(){
+        const myText = document.querySelector('#item')
+        myText.innerHTML = '<p>This is a blurb</p>'
+        
+    });
+
+    area7.addEventListener('mouseover', function(){
+        const myText = document.querySelector('#item')
+        myText.innerHTML = '<p>This is a blurb</p>'
+        
+    });
+
+    area8.addEventListener('mouseover', function(){
+        const myText = document.querySelector('#item')
+        myText.innerHTML = '<p>This is a blurb</p>'
+        
+    });
+
+    area9.addEventListener('mouseover', function(){
+        const myText = document.querySelector('#item')
+        myText.innerHTML = '<p>This is a blurb</p>'
+        
+    });
+   
+}) ();
