8dfdf7c4c0f33ef05926cf61d35ee5624a72c41d
Emma O'Connor <esoconnor@ucdavis.edu>
2025-03-04 11:38:14 -0800

    game on

--- everyPicture/v2/script.js
+++ everyPicture/v2/script.js
@@ -4,67 +4,161 @@
      console.log('reading js');
 
 
-    const area1 = document.querySelector('#1-area');
-    const area2 = document.querySelector('#2-area');
-    const area3 = document.querySelector('#3-area');
-    const area4 = document.querySelector('#4-area');
-    const area5 = document.querySelector('#5-area');
-    const area6 = document.querySelector('#6-area');
-    const area7 = document.querySelector('#7-area');
-    const area8 = document.querySelector('#8-area');
-    const area9 = document.querySelector('#9-area');
+    const area1 = document.querySelector('#area1');
+    const area2 = document.querySelector('#area2');
+    const area3 = document.querySelector('#area3');
+    const area4 = document.querySelector('#area4');
+    const area5 = document.querySelector('#area5');
+    const area6 = document.querySelector('#area6');
+    const area7 = document.querySelector('#area7');
+    const area8 = document.querySelector('#area8');
+    const area9 = document.querySelector('#area9'); 
 
-    area1.addEventListener('mouseover', function(){
-        const myText = document.querySelector('#item')
-        myText.innerHTML = '<p>This is a blurb</p>'
+    area1.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a book I was given after from my first internship.</p>';
 
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
         
     });
 
-    area2.addEventListener('mouseover', function(){
-        const myText = document.querySelector('#item')
-        myText.innerHTML = '<p>This is a blurb</p>'
+    area2.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a sweet Valentine a friend gifted to me</p>';
+
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
         
     });
 
-    area3.addEventListener('mouseover', function(){
-        const myText = document.querySelector('#item')
-        myText.innerHTML = '<p>This is a blurb</p>'
+    area3.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
         
     });
 
-    area4.addEventListener('mouseover', function(){
-        const myText = document.querySelector('#item')
-        myText.innerHTML = '<p>This is a blurb</p>'
+    area4.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
         
     });
 
-    area5.addEventListener('mouseover', function(){
+    area5.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
+    });
+
+    area6.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
         
     });
 
-    area6.addEventListener('mouseover', function(){
-        const myText = document.querySelector('#item')
-        myText.innerHTML = '<p>This is a blurb</p>'
+    area7.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
         
     });
 
-    area7.addEventListener('mouseover', function(){
-        const myText = document.querySelector('#item')
-        myText.innerHTML = '<p>This is a blurb</p>'
+    area8.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
         
     });
 
-    area8.addEventListener('mouseover', function(){
-        const myText = document.querySelector('#item')
-        myText.innerHTML = '<p>This is a blurb</p>'
+    area9.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
         
     });
 
-    area9.addEventListener('mouseover', function(){
-        const myText = document.querySelector('#item')
-        myText.innerHTML = '<p>This is a blurb</p>'
+    area10.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
         
     });
+
+    area11.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
+        
+    });
+
+    area12.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
+        
+    });
+
+    area13.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
+        
+    });
+
+    area14.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
+        
+    });
+
+    area15.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
+        
+    });
+
+    area16.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
+        
+    });
+
+    area17.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
+        
+    });
+
+    area18.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
+        
+    });
+
+    area19.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
+        
+    });
+
+    area20.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
+        
+    });
+
+    area21.addEventListener('click', function(){
+        const myText = document.querySelector('#item');
+        myText.innerHTML = '<p>This is a blurb</p>';
+        
+    });
+
+   
+
+
+
+
+
+    const close = document.querySelector('#close');
+    close.addEventListener('click',function(){
+        /* myText.style.display = "none"; */
+        document.querySelector('#item').style.display = "none";
+        document.querySelector('#close').style.display = "none";
+
+    });
+    
+
    
 }) ();
