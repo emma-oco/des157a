23b42db799b2929c92762bf5fe6d73ee4351d4cb
Emma O'Connor <esoconnor@ucdavis.edu>
2025-03-13 07:41:21 -0700

    project 3 and 4

--- everyPicture/v2/script.js
+++ everyPicture/v2/script.js
@@ -34,115 +34,156 @@
 
     area3.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>I bought these postcards in a museum in Barcelona. I love art nouveau and purchasing small items like postcards to remember my travels so these were perfect!</p>';
+
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
         
     });
 
     area4.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>When I’m given flowers on special occasions I like to dry them to display. I tend to be sentimental about any kind of gift anyone gives me and will do a lot to preserve it and the memory that comes with the item. </p>';
+
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
         
     });
 
     area5.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>This is a tag from a sweater my boyfriend gave me for Christmas. I absolutely love the sweater and wear it quite often. I’m planning on making a scrap book and incorporating objects like this and writing my memories down so I can keep them for years to come. </p>';
+
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
     });
 
     area6.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>My aunt got me this ring for my sweet sixteen. I’ll always remember going to pick it out and how much fun I had celebrating with with her.</p>';
         
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
     });
 
     area7.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>These postcards are from the Manetti Shrem exhibition Light Into Density that I worked on.</p>';
         
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
     });
 
     area8.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>I love saving business cards. This one is for a chocolate company that had a booth at a chocolate festival I attended. </p>';
         
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
     });
 
     area9.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>I love saving business cards. This one is for a furniture store in Davis. I ended up purchasing furniture from this store for another exhibition I worked on.</p>';
         
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
     });
 
     area10.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>For halloween, me and my housemates decorated little pumpkins and put them on our doors. I had gotten a package of halloween-themed wooden stickers from my mom and we all decided on which character fit each of us the best. This one is mine. </p>';
         
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
     });
 
     area11.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>I spent this past summer in San Diego with my aunt and was lucky enough to get to go to comic con. While I was walking around outside, one of the people cosplaying gave me this coin. It was such a cool experience because of how in-character he was!</p>';
         
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
     });
 
     area12.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>These are tickets for Kew Gardens. This past summer I went on a trip to London with my friends and we visited Kew. It was such an amazing experience and I highly recommend it to anyone that has the chance to go.</p>';
         
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
     });
 
     area13.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>These scissors have traveled far and wide with me. I’ve got them when I was little because I loved to craft but since then I’ve brought them to study abroad and college.</p>';
         
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
     });
 
     area14.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>This playbill is from a showI got to see in New York. I went there with my family this past summer from my Nana’s 80th birthday. She grew up in Brooklyn and wanted to see her old home again. </p>';
         
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
     });
 
     area15.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>While I Wass in New York, I got my Nana some yummy chocolates for her birthday. She loves sweets.</p>';
         
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
     });
 
     area16.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>My grandma always send me the cutest care packages for holidays filled with candy and other fun items. This is one of the card from my halloween package. </p>';
         
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
     });
 
     area17.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>When I visited Barcelona I got the Barcelona card and was able to visit 7 museums in one day. We walked from museum to museum and ended up with 30,000 steps. </p>';
         
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
     });
 
     area18.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>This match set is from my stay with my aunt this past summer. She loved lighting candles at the end of the day.</p>';
         
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
     });
 
     area19.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>This card is from a hostel I stayed at in Utrecht during a summer abroad trip I took two years ago. I have amazing memories from that trip and kept this card in my phone case for at least a year after I went on it. </p>';
         
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
     });
 
     area20.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>While I was in New York I got to visit Levain. I usually can’t get baked goods because I’m gluten free and was bummed because Levain is supposed to be one of the best, but they ended up having a gluten free version of their cookie! It was so good. </p>';
         
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
     });
 
     area21.addEventListener('click', function(){
         const myText = document.querySelector('#item');
-        myText.innerHTML = '<p>This is a blurb</p>';
+        myText.innerHTML = '<p>This button was given to me on my summer abroad trip for our first assignment. We were sorted into groups based on the color of button we were given. </p>';
         
+        myText.style.display = "block";
+        document.querySelector('#close').style.display = "block";
     });
 
    
